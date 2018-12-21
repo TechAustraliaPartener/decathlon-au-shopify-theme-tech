e8a45d0e806239217c0922d9a3736d12b5becaf3
kristen-loyd-decathlon <kristen.loyd@decathlon.com>
2018-12-20 20:15:03 -0800

    Remove storeAddressRef, no longer being used

--- assets/account--order.js
+++ assets/account--order.js
@@ -1,27 +1,3 @@
-var storeAddressRef = {
-    ////
-    //  TODO:
-    //      Need a better solution for this info, maybe a store database? (or do we have this already in NewStore?)
-    //      The same information in is shopify-decathlon-proxy
-    //  NOTE:
-    //      Keys are created after NewStore's model for the San Francisco store: STSF001
-    //      Format: ST + (2 letters representing location) + (number representing the order of store opening)
-    //
-    //   This same object can be found in the stores module of shopify-decathlon-proxy
-    ////
-    STSF001: {
-      title: "Decathlon San Francisco Store",
-      address_line_1: "735 Market St",
-      address_line_2: "",
-      city_state_zip: "San Francisco, CA 94103"
-    },
-    STEM001: {
-      title: "Decathlon Emeryville Store",
-      address_line_1: "3938 Horton St",
-      address_line_2: "",
-      city_state_zip: "Emeryville, CA 94608"
-    }
-}
 $(document).ready(function () {
   // Set up variables
   var productDict = {}
