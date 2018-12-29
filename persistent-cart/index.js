a01a4ac6709820d326a7e8342c1bf1486186ddbd
Caleb Eby <caleb.eby01@gmail.com>
2018-12-28 17:06:04 -0800

    Add set cart on login

--- persistent-cart/index.js
+++ persistent-cart/index.js
@@ -32,3 +32,25 @@ makeRequest(
     }
   `
 );
+
+const setCartCookie = cartId => {
+  document.cookie = `cart=${cartId}; path=/`;
+};
+
+const deleteCartCookie = () => {
+  document.cookie = `cart=; expires=Thu, 01 Jan 1970 00:00:01 GMT`;
+};
+
+const customerLoginForm = document.querySelector('#CustomerLoginForm > form');
+
+if (customerLoginForm) {
+  customerLoginForm.addEventListener('submit', () => {
+    setCartCookie('d3b0b5de56274bea158ddf8e348802be');
+  });
+}
+
+const logoutLink = document.querySelector('.accountLogout-link');
+
+if (logoutLink) {
+  logoutLink.addEventListener('click', deleteCartCookie);
+}
