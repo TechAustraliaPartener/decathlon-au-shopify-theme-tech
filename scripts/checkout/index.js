df171b3c83e66d3ab7f3ba5f3efebebef2b38e3b
Gerardo Rodriguez <mr.gerardo.rodriguez@gmail.com>
2019-01-29 14:10:50 -0800

    PR feedback

--- scripts/checkout/index.js
+++ scripts/checkout/index.js
@@ -7,29 +7,26 @@ const shipToggleBtn = querySelector('.js-toggle-shipping');
 const pickupToggleBtn = querySelector('.js-toggle-pickup');
 
 /**
- * Input fields of interest
  * Note: If Shopify updates the `content_for_layout` Liquid drop,
  * there is the possiblity the selectors will fail. This is a known
  * risk we are taking.
  * @see https://help.shopify.com/en/themes/development/layouts/checkout/best-practices#dom-dependency-and-liquid-drops
  * @see https://docs.decathlon.us/shopify/-LSqQDiLEYzw_Jv4oueW/product-feature/untitled-2#shopify-plus-checkout-customization
  */
-const addressInput = querySelector('[data-address-field="address1"]');
-const cityInput = querySelector('[data-address-field="city"]');
-const countryInput = querySelector('[data-address-field="country"]');
-const provinceInput = querySelector('[data-address-field="province"]');
-const zipInput = querySelector('[data-address-field="zip"]');
+const toggleElementSelectors = {
+  addressInput: '[data-address-field="address1"]',
+  cityInput: '[data-address-field="city"]',
+  countryInput: '[data-address-field="country"]',
+  provinceInput: '[data-address-field="province"]',
+  zipInput: '[data-address-field="zip"]'
+};
 
 /**
- * Grouping all toggle elements together for easier access
+ * Grouping all toggle elements together into an array for easier access
  */
-const toggleElements = [
-  addressInput,
-  cityInput,
-  countryInput,
-  provinceInput,
-  zipInput
-];
+const toggleElements = Object.keys(toggleElementSelectors).map(
+  key => querySelector(toggleElementSelectors[key])
+);
 
 /**
  * Bind the toggle buttons
