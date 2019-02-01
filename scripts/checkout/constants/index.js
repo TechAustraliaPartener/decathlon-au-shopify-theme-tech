3e97258234538b2e7229eb85f673a59a05ed9de4
Gerardo Rodriguez <mr.gerardo.rodriguez@gmail.com>
2019-02-01 15:15:50 -0800

    Add app state + constants

---
+++ scripts/checkout/constants/index.js
@@ -0,0 +1,5 @@
+import deliveryMethods from './delivery-methods';
+import checkoutSteps from './checkout-steps';
+
+export const DELIVERY_METHODS = deliveryMethods;
+export const CHECKOUT_STEPS = checkoutSteps;
