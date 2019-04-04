230b4163d0065591b7e1dbf09d9fd2088a83ecba
Jason Honey <jason@sidedishstudio.com>
2019-04-04 11:23:32 -0700

    Feature: Auto-click "same as shipping" (#222)
    * Auto-click same as shipping and hide spinner on out of stock
    
    * Make stock_problems a page
    
    * Remove console log
    
    * Remove spinner from Order Status page

---
+++ scripts/checkout/stock-problems/ui-elements.js
@@ -0,0 +1,4 @@
+import SELECTORS from './selectors';
+
+export const loadingOverlay = document.querySelector(SELECTORS.LOADING_OVERLAY);
+export const loadingImage = document.querySelector(SELECTORS.LOADING_IMAGE);
