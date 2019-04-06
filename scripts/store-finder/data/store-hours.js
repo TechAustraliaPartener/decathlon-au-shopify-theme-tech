7dc12976ad29f62967598d4a3646cf390ab4ec94
Juma Stevens <jumastevens@gmail.com>
2019-04-05 17:19:33 -0700

    Bugfix: Dynamic store hours
    * Address store hour times via dynamic output, accounting for timezone/daylight savings time
    
    * Update store hours

---
+++ scripts/store-finder/data/store-hours.js
@@ -0,0 +1,27 @@
+/**
+ * - Store properties range from 0 (Sunday) - 6 (Saturday)
+ * - Store values are in Military Time
+ * - "adr_GezSSC9M"/"adr_K6s3Kaja" are the IDs of each store
+ * found in ./stores.js
+ * - "0: [9, 19]" is equivalent to "Sunday: [9am, 7pm]"
+ */
+export default {
+  adr_GezSSC9M: {
+    0: [9, 19],
+    1: [9, 20],
+    2: [9, 20],
+    3: [9, 20],
+    4: [9, 20],
+    5: [9, 20],
+    6: [9, 20]
+  },
+  adr_K6s3Kaja: {
+    0: [9, 21],
+    1: [9, 21],
+    2: [9, 21],
+    3: [9, 21],
+    4: [9, 21],
+    5: [9, 21],
+    6: [9, 21]
+  }
+};
