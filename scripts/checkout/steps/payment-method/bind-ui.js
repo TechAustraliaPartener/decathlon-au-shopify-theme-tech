606fac8ec103c95667f0ea5c837bea1410127fd1
Jason Honey <jason@sidedishstudio.com>
2019-04-08 10:12:13 -0700

    Feature: Clear auto-populated billing address (#236)
    * Clear Auto-filled Billing Address
    
    * Clear on SH and PU
    
    * Add elementExists protection

--- scripts/checkout/steps/payment-method/bind-ui.js
+++ scripts/checkout/steps/payment-method/bind-ui.js
@@ -3,13 +3,31 @@ import {
   shipToLabel,
   shipToMap,
   loadingOverlay,
-  loadingImage
+  loadingImage,
+  userAddress1,
+  userAddress2,
+  userCity,
+  userZip
 } from './ui-elements';
 import STATE from '../../state';
 import { DELIVERY_METHODS } from '../../constants';
 import { hideElements, elementExists } from '../../ui-helpers';
 
+/**
+ * Clear auto-filled billing address fields
+ */
+const clearBillingAddress = () => {
+  const billingFields = [userAddress1, userAddress2, userCity, userZip];
+  billingFields.forEach(field => {
+    if (elementExists(field)) {
+      field.value = '';
+    }
+  });
+};
+
 const bindUI = () => {
+  // Clear auto-filled billing address fields
+  clearBillingAddress();
   hideElements([loadingOverlay, loadingImage]);
   if (STATE.deliveryMethod === DELIVERY_METHODS.PICKUP) {
     hideElements(billingAddressChoices);
