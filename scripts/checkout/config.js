10c1bc42d88414e634062c301be47a2377738edb
Jason Honey <jason@sidedishstudio.com>
2019-04-15 16:45:29 -0700

    Update method case (#280) (#281)

--- scripts/checkout/config.js
+++ scripts/checkout/config.js
@@ -12,8 +12,8 @@ const ifDev = values =>
     : values[1];
 
 const config = {
-  PICKUP_SHIPPING_METHODS: ['Shopify-pickup-0.00'],
-  PICKUP_SHIPPING_METHOD: 'Shopify-pickup-0.00',
+  PICKUP_SHIPPING_METHODS: ['shopify-Pickup-0.00'],
+  PICKUP_SHIPPING_METHOD: 'shopify-Pickup-0.00',
   CLASSES: {
     ACTIVE_SHIPPICK_BTN: 'js-de-active-pickship-btn',
     ACTIVE_PICKUP_LOCATION: 'js-de-active-location',
