18d0248751e57b9e6036eb6667cd5bbf80f9d01c
Josh Schneider <josh68@gmail.com>
2019-04-18 10:55:25 -0700

    Chore: Update ratings and reviews to use static data (#290)
    • Revise product-related `liquid` files to use new `json_string`-type `recent_reviews_object` data
    • Revise JS to not run on page load, prepare for dynamic review loading, sorting, and filtering

--- scripts/product-page/constants.js
+++ scripts/product-page/constants.js
@@ -26,6 +26,11 @@ export const IS_ACTIVE_CLASS = `${CSS_PREFIX}is-active`;
 export const REVIEW_VOTE_CLASS = `${JS_PREFIX}CustomerReviewVote`;
 
 /**
+ * The CSS class on the button used to get more reviews
+ */
+export const MORE_REVIEWS_CLASS = `${JS_PREFIX}moreReviews`;
+
+/**
  * Reusable values
  */
 
@@ -35,9 +40,19 @@ export const REVIEW_VOTE_CLASS = `${JS_PREFIX}CustomerReviewVote`;
 export const STAR_RATING_PERCENTAGE_MULTIPLIER = 20;
 
 /**
- * Returns a base URL for querying the Decathlon Reviews API
- * @param {string} modelCode
- * @returns {string} URL with modelCode
+ * Base URL for querying the Decathlon Reviews API
  */
-export const GET_REVIEWS_BASE_URL = (modelCode = '') =>
-  `https://reviews.decathlon.com/api/en_US/review/list?site=1132&type=1&locales=en&nb=3&offer=${modelCode}`;
+export const REVIEWS_BASE_URL =
+  'https://reviews.decathlon.com/api/en_US/review/list';
+
+const REVIEWS_DEFUAULT_NUMBER_PER_PAGE = 5;
+
+/**
+ * Base query parameters for the Decathlon Reviews API
+ */
+export const REVIEWS_BASE_QUERY_PARAMS = {
+  site: 1132,
+  type: 1,
+  locales: 'en',
+  nb: REVIEWS_DEFUAULT_NUMBER_PER_PAGE
+};
