c85c5803b047fe23ef5424b74ad515571c07848d
Gerardo Rodriguez <gerardo@cloudfour.com>
2019-04-26 16:15:57 -0700

    Chore: Decouple MasterSelect & window.productJSON logic (#322)
    Changes include:
    • created and moved master `<select>` logic to `MasterSelect` module
    • created and moved `window.productJSON` logic to `ProductJSON` module
    • refactored `SizeSwatches` & `ColorSwatches` dispatched data to use `color` and `size` instead of `value`
    • refactored `index.js` to add `init()`, `updateUI()` and `setUpListeners()` functions for better organization
    • added a `state` object to the `SizeSwatches` & `ColorSwatches` to allow each to keep track of their respective state
    • removed Default Exports in favor of Named Exports for `SizeSwatches` & `ColorSwatches`

--- scripts/product-page/option-states.js
+++ scripts/product-page/option-states.js
@@ -6,23 +6,21 @@
  */
 
 import {
-  IS_SOLD_OUT_TAG,
-  IS_ACTIVE_CLASS,
   IS_OUT_OF_STOCK_CLASS,
   IS_SOLD_OUT_CLASS,
   JS_PREFIX
 } from './constants';
+import {
+  isSoldOut,
+  getAvailableColorsFromSize,
+  getAvailableSizesFromColor
+} from './product-data';
 import $ from 'jquery';
 
-const productsJSON = window.productJSON;
-const variantsJSON = productsJSON.variants;
-const colorIndex = productsJSON.options.indexOf('Color');
-const sizeIndex = productsJSON.options.indexOf('Size');
-const COLOR_OPTION = `option${colorIndex + 1}`;
-const SIZE_OPTION = `option${sizeIndex + 1}`;
+// @todo Move color & size swatches logic out of this module and into
+// the `ColorSwatches` & `SizeSwatches` modules
 const colorSwatchesSelector = `.${JS_PREFIX}ColorSwatches-option`;
 const sizeSwatchesSelector = `.${JS_PREFIX}SizeSwatches-option`;
-const $masterSelect = $('#productSelect');
 
 /**
  * Checks product attributes for the "End of Life" tag and specifies whether the
@@ -30,126 +28,44 @@ const $masterSelect = $('#productSelect');
  *
  * @returns {string} A CSS class
  */
-function getUnavailableCssClass() {
-  if (productsJSON.tags.includes(IS_SOLD_OUT_TAG)) {
+const getUnavailableCssClass = () => {
+  if (isSoldOut()) {
     return IS_SOLD_OUT_CLASS;
   }
   return IS_OUT_OF_STOCK_CLASS;
-}
-
-/**
- * Finds variant id based on size and color selection and updated
- * master variant select element
- * @param {string} selectedSize Value of the selected size option
- * @param {string} selectedColor Value of the selected color option
- */
-function updateMasterSelect(selectedSize, selectedColor) {
-  const selectedVariant = variantsJSON.find(
-    variant =>
-      variant[SIZE_OPTION] === selectedSize &&
-      variant[COLOR_OPTION] === selectedColor
-  );
-  if (selectedVariant) {
-    $masterSelect.val(selectedVariant.id);
-  }
-}
-
-/**
- * Returns the selected value for the given option
- * @param {string} The CSS class name of the option
- * @returns {string} The option value
- */
-function getSelectedOptionValue(cssClassOptionName) {
-  const $option = $(cssClassOptionName);
-
-  if ($option.size() > 0) {
-    return $option.first().val();
-  }
-  return '';
-}
-
-/**
- * Filters available variants
- */
-const availableVariants = variantsJSON.filter(variant => variant.available);
-
-/**
- * Returns the available colors given a size
- *
- * @param {string} size A size option
- * @returns {Array} Available colors
- */
-function getAvailableColorsFromSize(size) {
-  return availableVariants.reduce((accArray, variant) => {
-    if (!accArray.includes(variant[COLOR_OPTION])) {
-      if (size) {
-        if (size === variant[SIZE_OPTION]) {
-          accArray.push(variant[COLOR_OPTION]);
-        }
-      } else {
-        accArray.push(variant[COLOR_OPTION]);
-      }
-    }
-    return accArray;
-  }, []);
-}
-
-/**
- * Returns the available sizes given a color
- *
- * @param {string} color A color option
- * @returns {Array} Available sizes
- */
-function getAvailableSizesFromColor(color) {
-  return availableVariants.reduce((accArray, variant) => {
-    if (!accArray.includes(variant[SIZE_OPTION])) {
-      if (color) {
-        if (color === variant[COLOR_OPTION]) {
-          accArray.push(variant[SIZE_OPTION]);
-        }
-      } else {
-        accArray.push(variant[SIZE_OPTION]);
-      }
-    }
-    return accArray;
-  }, []);
-}
+};
 
 /**
  * Updates the "unavailable" state by adding/removing a CSS class
  *
+ * @todo Can this logic be handled in the ColorSwatches/SizeSwatches modules?
  * @param {string} optionCssClassName The options CSS class name
  * @param {Array} enabledOptions The enabled options to check against
  */
-function updateOptionUnavailableState(optionCssClassName, enabledOptions) {
-  const unavailableClass = getUnavailableCssClass();
+const updateOptionUnavailableState = (optionCssClassName, enabledOptions) => {
+  // @todo Consider moving this logic to `ColorSwatches` & `SizeSwatches` modules
   $(optionCssClassName).each(function() {
     const isUnavailable = !enabledOptions.includes($(this).val());
-    $(this).toggleClass(unavailableClass, isUnavailable);
+    $(this).toggleClass(getUnavailableCssClass(), isUnavailable);
   });
-}
+};
 
-export function updateOptionStates() {
-  // Get value of selected color
-  const selectedColor = getSelectedOptionValue(
-    `${colorSwatchesSelector}.${IS_ACTIVE_CLASS}`
-  );
-
-  // Get value of selected size
-  const selectedSize = getSelectedOptionValue(
-    `${sizeSwatchesSelector}.${IS_ACTIVE_CLASS}`
-  );
-
-  // Calculate available sizes and colors based on selected swatches
-  const availableColors = getAvailableColorsFromSize(selectedSize);
-  const availableSizes = getAvailableSizesFromColor(selectedColor);
-
+/**
+ * Updates the color & size options "available" states
+ *
+ * @todo Consider moving this logic to `ColorSwatches` & `SizeSwatches` modules
+ * @param {Object} obj The state object
+ * @param {string} obj.size The currently selected size
+ * @param {string} obj.color The currently selected color
+ */
+export const updateOptionStates = ({ size, color }) => {
   // Update swatches with classes to display state in UI
-  updateOptionUnavailableState(sizeSwatchesSelector, availableSizes);
-  updateOptionUnavailableState(colorSwatchesSelector, availableColors);
-
-  // Get variant ID and update master select element to set the variant that will add to cart
-  if (selectedSize && selectedColor) {
-    updateMasterSelect(selectedSize, selectedColor);
-  }
-}
+  updateOptionUnavailableState(
+    sizeSwatchesSelector,
+    getAvailableSizesFromColor(color)
+  );
+  updateOptionUnavailableState(
+    colorSwatchesSelector,
+    getAvailableColorsFromSize(size)
+  );
+};
--- scripts/product-page/size-swatches.js
+++ scripts/product-page/size-swatches.js
@@ -3,8 +3,15 @@
  */
 
 import { IS_ACTIVE_CLASS, JS_PREFIX } from './constants';
+// @todo Consider removing jQuery dependency
 import $ from 'jquery';
-import { updateOptionStates } from './option-states';
+
+/**
+ * Module state
+ */
+let state = {
+  size: null
+};
 
 /**
  * Module-specific constants
@@ -16,7 +23,7 @@ const CLICK_EVENT = 'click';
 /**
  * Root element
  */
-const $SizeSwatches = $(`.${JS_PREFIX}SizeSwatches`);
+export const $SizeSwatches = $(`.${JS_PREFIX}SizeSwatches`);
 
 /**
  * Children elements
@@ -27,57 +34,95 @@ const $SizeSwatchesOptions = $SizeSwatches.find(
 const $SizeInfo = $(`.${JS_PREFIX}SizeInfo`);
 
 /**
+ * Updates the module state
+ *
+ * @param {Object} newState The new state
+ * @returns {Object} The up-to-date state
+ */
+const updateState = newState => {
+  // Assign the new state to the old state
+  state = {
+    ...state,
+    ...newState
+  };
+  // For convenience, return the updated state
+  return state;
+};
+
+/**
  * Trigger $SizeSwatches specific events
  *
+ * @param {Object} state The current state
  * @fires SizeSwatches:select
  */
-function notifyListeners() {
+const notifyListeners = state => {
   /**
    * The SizeSwatches "select" event
    *
    * @event SizeSwatches:select
    * @type {object}
-   * @property {string} value The size value selected
+   * @property {string} size The size value selected
    */
-  $SizeSwatches.trigger(SELECT_EVENT, { value: $(this).val() });
-}
-
-/**
- * Handles UI updates for the Size Swatches component
- */
-function updateUI() {
-  selectSize.call(this);
-  $SizeInfo.text($(this).val());
-  updateOptionStates();
-}
+  $SizeSwatches.trigger(SELECT_EVENT, state);
+};
 
 /**
  * Updates the selected size UI state
+ *
+ * @param {Element} selectedOption The selected option HTML element
  */
-function selectSize() {
+const updateSizeUiState = selectedOption => {
   // Visually unselect all options
   $SizeSwatchesOptions.removeClass(IS_ACTIVE_CLASS);
   // Then visually select the current option
-  $(this).addClass(IS_ACTIVE_CLASS);
-}
+  $(selectedOption).addClass(IS_ACTIVE_CLASS);
+};
+
+/**
+ * Handles UI updates for the Size Swatches component
+ *
+ * @param {Object} obj
+ * @param {Object} obj.state The current state
+ * @param {Element} obj.selectedOption The selected option HTML element
+ */
+const updateUI = ({ state, selectedOption }) => {
+  // We need to update the selected color option UI state
+  updateSizeUiState(selectedOption);
+  // We need to update the selected color text
+  $SizeInfo.text(state.size);
+};
 
 /**
  * Handler for when a size choice is made
+ *
+ * @todo Consider removing jQuery dependency
+ * @this $SizeSwatchesOptions The triggered size swatch option element
  */
-function onSizeSelect() {
-  notifyListeners.call(this);
-  updateUI.call(this);
-}
+const onSizeSelect = function() {
+  const newState = updateState({
+    // @todo Consider removing jQuery dependency
+    size: $(this).val()
+  });
+
+  notifyListeners(newState);
+  updateUI({
+    state: newState,
+    selectedOption: this
+  });
+};
+
+/**
+ * Helper to retrieve the module state
+ *
+ * @returns {Object} The module state
+ */
+export const getState = () => state;
+
 /**
  * Initializes functionality by setting up event binding
  *
- * @returns {jQuery} The root jQuery $SizeSwatches object
+ * @todo Consider removing jQuery dependency
  */
-function init() {
+export const init = () => {
   $SizeSwatchesOptions.on(CLICK_EVENT, onSizeSelect);
-  return $SizeSwatches;
-}
-
-export default {
-  init
 };
