e846ce6bffb231726b44cd7a32adb5a0a43f9055
Caleb Eby <calebeby@users.noreply.github.com>
2019-07-11 15:17:09 -0700

    Make the rollup incremental rebuilds faster (#638)

--- scripts/build.js
+++ scripts/build.js
@@ -10,6 +10,7 @@ const rollupNodeResolve = require('rollup-plugin-node-resolve');
 const rollupCommonJs = require('rollup-plugin-commonjs');
 const babel = require('rollup-plugin-babel');
 const { terser } = require('rollup-plugin-terser');
+const changed = require('gulp-changed');
 
 const babelConfig = require('./babel.config');
 
@@ -18,48 +19,54 @@ const taskName = 'jsC4Scripts';
 const prod = process.env.NODE_ENV === 'production';
 const BUILT_PREFIX = 'built-';
 
+const DESTINATION = './assets';
+
 module.exports = gulp => {
   gulp.task(taskName, () => {
-    return gulp
-      .src('scripts/*/index.js')
-      .pipe(
-        gulpRollup(
-          {
-            rollup,
-            plugins: [
-              rollupNodeResolve(),
-              rollupCommonJs(),
-              babel({ babelrc: false, ...babelConfig, exclude: [/core-js/] }),
-              terser({
-                compress: {
-                  passes: 4,
-                  unsafe: true,
-                  pure_getters: true,
-                  join_vars: prod
-                },
-                mangle: prod,
-                output: { beautify: !prod }
-              })
-            ],
-            external: ['jquery', 'handlebars']
-          },
-          {
-            format: 'iife',
-            globals: {
-              jquery: 'jQuery',
-              handlebars: 'Handlebars'
+    return (
+      gulp
+        .src('scripts/*/index.js')
+        .pipe(
+          gulpRollup(
+            {
+              rollup,
+              plugins: [
+                rollupNodeResolve(),
+                rollupCommonJs(),
+                babel({ babelrc: false, ...babelConfig, exclude: [/core-js/] }),
+                prod &&
+                  terser({
+                    compress: {
+                      passes: 4,
+                      unsafe: true,
+                      pure_getters: true
+                    },
+                    mangle: true
+                  })
+              ],
+              external: ['jquery', 'handlebars']
+            },
+            {
+              format: 'iife',
+              globals: {
+                jquery: 'jQuery',
+                handlebars: 'Handlebars'
+              }
             }
-          }
+          )
         )
-      )
-      .pipe(
-        rename(path => {
-          // Rename from /scripts/asdf/index.js to /scripts/asdf.js
-          path.basename = `${BUILT_PREFIX}${path.dirname}`;
-          path.dirname = './';
-        })
-      )
-      .pipe(gulp.dest('assets'));
+        .pipe(
+          rename(path => {
+            // Rename from /scripts/asdf/index.js to /scripts/built-asdf.js
+            path.basename = `${BUILT_PREFIX}${path.dirname}`;
+            path.dirname = './';
+          })
+        )
+        // We don't want files to be written to `assets` if they are not different.
+        // Otherwise themekit will re-upload them
+        .pipe(changed(DESTINATION, { hasChanged: changed.compareContents }))
+        .pipe(gulp.dest(DESTINATION))
+    );
   });
 
   return taskName;
