00178f1e1cd92e178487b82fdb8971ab32d31a77
Josh Schneider <josh68@gmail.com>
2019-07-26 17:08:59 -0700

    Bugfix: Revise NodeList forEach polyfill, avoid destructuring NodeList (#696)

--- scripts/utilities/foreach-polyfill.js
+++
@@ -1,7 +0,0 @@
-/**
- * Polyfill for IE to support NodeList.foreach
- * https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach#Polyfill
- */
-if (window.NodeList && !NodeList.prototype.forEach) {
-  NodeList.prototype.forEach = Array.prototype.forEach;
-}
