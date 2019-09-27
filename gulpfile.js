1a397c42694053a6989e680cde87d5f2ce00dfaa
John <john@createur.com.au>
2019-09-27 14:59:09 +1000

    16qmu8: account extra details liquid and snippets

--- gulpfile.js
+++ gulpfile.js
@@ -159,13 +159,11 @@ gulp.task(
 
 gulp.task('styles', function() {
   return gulp
-    .src(
-      [
-        `${STYLES_PATH}/product-page/index.scss`,
-        `${STYLES_PATH}/product-tile/index.scss`
-      ],
-      { base: STYLES_PATH }
-    )
+    .src([
+      `${STYLES_PATH}/product-page/index.scss`,
+      `${STYLES_PATH}/product-tile/index.scss`,
+      `${STYLES_PATH}/customer-fields/index.scss`
+    ], { base: STYLES_PATH })
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(
       rename(path => {
---
+++ scripts/customer-fields/index.js
@@ -0,0 +1,84 @@
+import $ from 'jquery';
+
+$(document).ready(() => {
+  $('#accountInfo input, #accountEdit input').on('change', function() {
+    const $registerField = $(this);
+    const $hiddenCustomerField = $(
+      `#customer-fields input[name="${$registerField.attr('name')}"]`
+    );
+    if ($hiddenCustomerField.length > 0) {
+      if ($registerField.attr('type') === 'checkbox') {
+        $hiddenCustomerField.prop('checked', $registerField.prop('checked'));
+      } else if ($registerField.attr('type') === 'select') {
+        $hiddenCustomerField.val($registerField.val());
+      } else {
+        $hiddenCustomerField.val($registerField.val());
+      }
+    }
+  });
+
+  $('#accountInfo select, #accountEdit select').on('change', function() {
+    const $registerField = $(this);
+    const $hiddenCustomerField = $(
+      `#customer-fields select[name="${$registerField.attr('name')}"]`
+    );
+    if ($hiddenCustomerField.length > 0) {
+      $hiddenCustomerField.val($registerField.val());
+    }
+  });
+
+  window.addSpace = function($input) {
+    $input.val(`${$input.val()} `);
+  };
+
+  $('.general-popup .modal_close').on('click', function() {
+    const $popup = $(this).parents('.general-popup');
+    $popup.hide();
+    if ($popup.attr('id') === 'welcomePopup') {
+      location.reload();
+    }
+  });
+
+  $('.general-popup .modal_background').on('click', function() {
+    const $popup = $(this).parents('.general-popup');
+    $popup.hide();
+    if ($popup.attr('id') === 'welcomePopup') {
+      location.reload();
+    }
+  });
+
+  $('[data-open-popup]').on('click', function() {
+    const $popup = $($(this).attr('href'));
+    $popup.show();
+  });
+
+  $('[data-submit]').on('submit', function(e) {
+    e.preventDefault();
+    if ($($(this).data('submit')).length > 0) {
+      $($(this).data('submit')).submit();
+    }
+  });
+
+  $('#editDetails').click(function(e) {
+    e.preventDefault();
+    $('#detailsDisplay').fadeOut(200, function() {
+      $('#accountEdit').fadeIn();
+    });
+  });
+
+  $('#updateMarketing').click(function(e) {
+    e.preventDefault();
+    $('[data-marketing-hide]').hide();
+    $('#detailsDisplay').fadeOut(200, function() {
+      $('#accountEdit').fadeIn();
+    });
+  });
+
+  $('#cancelEditAccount').click(function(e) {
+    e.preventDefault();
+    $('#accountEdit').fadeOut(200, function() {
+      $('[data-marketing-hide]').show();
+      $('#detailsDisplay').fadeIn();
+    });
+  });
+});
