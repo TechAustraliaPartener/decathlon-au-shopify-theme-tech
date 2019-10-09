06704ae2993dc7bd31d98861b489ee545630adb7
John <john@createur.com.au>
2019-10-09 16:52:10 +1100

    16tdjv: cart initialisation initial

--- scripts/cart-page/index.js
+++ scripts/cart-page/index.js
@@ -3,13 +3,17 @@
 import $ from 'jquery';
 import Vue from 'vue/dist/vue.esm.js';
 
+function supplementCart(cart, inventory) {
+  return inventory;
+}
+
 /**
  * Attach listeners to open collapse elements
  */
 const initCartDisplay = cart => {
   window.cartDisplay = new Vue({
     el: '#cartDisplay',
-    data: JSON.parse(JSON.stringify(cart)),
+    data: supplementCart(cart),
     methods: {
       changeWholeData(newData) {
         Object.keys(this.$data).forEach(key => (this.$data[key] = null));
@@ -28,13 +32,33 @@ const initCartDisplay = cart => {
   });
 };
 
+let cartReady = false;
+let invReady = false;
+
 $(document).on('cart.ready', function(event, cart) {
-  console.log('initCartDisplay');
-  initCartDisplay(cart);
+  cartReady = true;
+  console.log('CART READY', event, cart);
+  tryInit();
 });
 
+document.addEventListener('tomitLoaded', function(inventory) {
+  window.tomitProductInventoryInfo
+    .getProductsInventoryInformation(window.vars.tomitCartPayload)
+    .then(function(e, data) {
+      invReady = true;
+      console.log('INV READY', inventory);
+      tryInit();
+    });
+});
+
+function tryInit() {
+  if (cartReady && invReady) {
+    initCartDisplay(CartJS.cart);
+  }
+}
+
 $(document).on('cart.requestComplete', function(event, cart) {
-  window.cartDisplay.changeWholeData(cart);
+  window.cartDisplay.changeWholeData(supplementCart(cart));
   $('.js-de-cart__subtotal').text(Shopify.formatMoney(cart.total_price));
   $('.afterpay-info strong').text(Shopify.formatMoney(cart.total_price / 4));
   $('#CartCount').text(cart.item_count);
