50f4a1924b6d6240e757b45193fb538a70f62bdb
Mark Jamer Krout <mark.krout@createur.com.au>
2020-10-15 15:39:53 +0800

    #4nx1de -  added validation to the form fields

--- scripts/customer-fields/index.js
+++ scripts/customer-fields/index.js
@@ -65,13 +65,102 @@ $(document).ready(() => {
   $('[data-submit]').on('submit', function (e) {
     console.log('submit form');
     let error = false;
-    const province = $('select[name="customer[addresses][][province]"]');
+    const $first_name = $('input[name="customer[first_name]"]');
+    const $last_name = $('input[name="customer[last_name]"]');
+    const $gender = $('select[name="customer[gender]"]');
+    const $province = $('select[name="customer[addresses][][province]"]');
+    const $preferred_store = $('select[name="customer[preferred_store]"]');
+    const $birthday = $('input[name="customer[birthday]"]');
+    const $phone = $('input[name="customer[addresses][][phone]"]');
+    const $address1 = $('input[name="customer[addresses][][address1]"]');
+    const $post_code = $('input[name="customer[postcode]"]');
+
     e.preventDefault();
     if ($($(this).data('submit')).length > 0) {
+
+      if ($first_name.val() == '') {
+        $first_name.addClass('error');
+        error = true;
+      } else {
+        $first_name.removeClass('error');
+      }
+
+      if ($address1.val() == '') {
+        $address1.addClass('error');
+        error = true;
+      } else {
+        $address1.removeClass('error');
+      }
+
+      if ($post_code.val() == '') {
+        $post_code.addClass('error');
+        error = true;
+      } else {
+        $post_code.removeClass('error');
+      }
+
+      if ($phone.val() == '') {
+        $phone.addClass('error');
+        error = true;
+      } else {
+        $phone.removeClass('error');
+      }
+
+      // $phone.keyup(function () {
+      //   this.value = this.value.replace(/[^0-9\.]/g, '');
+      // });
+
+      // $('input[name="customer[addresses][][phone]"]').on('keypress', function (e) {
+      //   console.log('numbers only');
+      //   if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+      //     console.log('numbers only please');
+      //     return false;
+      //   }
+      // });
+
+      if ($last_name.val() == '') {
+        $last_name.addClass('error');
+        error = true;
+      } else {
+        $last_name.removeClass('error');
+      }
+
+      if ($gender.val() == '') {
+        $gender.addClass('error');
+        error = true;
+      } else {
+        $gender.removeClass('error');
+      }
+
+      if ($birthday.val() == '') {
+        $birthday.addClass('error');
+        error = true;
+      } else {
+        $birthday.removeClass('error');
+      }
+
+      if ($preferred_store.val() == '') {
+        $preferred_store.addClass('error');
+        error = true;
+      } else {
+        $preferred_store.removeClass('error');
+      }
+
+      if ($province.val() == '') {
+        $province.addClass('error');
+        error = true;
+      }
+      else {
+        $province.removeClass('error');
+      }
+
       if (!$('input[name="customer[accepts_terms_conditions]"]').is(':checked')) {
         $('input[name="customer[accepts_terms_conditions]"]').addClass('error')
         error = true;
+      } else {
+        $('input[name="customer[accepts_terms_conditions]"]').removeClass('error')
       }
+
       if (!error) {
         $($(this).data('submit')).submit();
       }
