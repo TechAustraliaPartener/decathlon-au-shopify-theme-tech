3117367050ec1800eff3489137d7c9e26e00500a
Mark Krout <mark.krout@createur.com.au>
2021-02-09 23:44:08 +0800

    #2ur73v - form validation and functionalities

--- scripts/customer-fields/index.js
+++ scripts/customer-fields/index.js
@@ -1,95 +1,170 @@
 import Cookies from 'js-cookie';
 import $ from 'jquery';
 
-$(document).ready(() => {
-  $('#accountInfo input, #accountEdit input').on('change', function () {
-    const $registerField = $(this);
-    const $hiddenCustomerField = $(
-      `#customer-fields input[name="${$registerField.attr('name')}"]`
-    );
-    if ($hiddenCustomerField.length > 0) {
-      if ($registerField.attr('type') === 'checkbox') {
-        $hiddenCustomerField.prop('checked', $registerField.prop('checked'));
-      } else if ($registerField.attr('type') === 'select') {
-        $hiddenCustomerField.val($registerField.val());
-      } else {
+$(document).ready(function () {
 
-        const prefix = $registerField.data('prefix');
-        const val = prefix ? prefix + $registerField.val() : $registerField.val();
+  $('#cf_accept_terms').on('change', function () {
+    if ($('#cf_accept_terms').is(':checked')) {
+      $('#cf_accept_terms').prop("checked", true);
+      $('#cf_accept_terms').val('true')
+    } else {
+      $('#cf_accept_terms').prop("checked", false);
+      $('#cf_accept_terms').val('')
+    }
+  })
 
-        $hiddenCustomerField.val(val);
+  function getCookie(name) {
+    var cookieArr = document.cookie.split(";");
+    for (var i = 0; i < cookieArr.length; i++) {
+      var cookiePair = cookieArr[i].split("=");
+
+      if (name == cookiePair[0].trim()) {
+        return decodeURIComponent(cookiePair[1]);
       }
     }
-  });
-
-  $('#accountInfo select, #accountEdit select').on('change', function () {
-    const $registerField = $(this);
-    const $hiddenCustomerField = $(
-      `#customer-fields select[name="${$registerField.attr('name')}"]`
-    );
-    if ($hiddenCustomerField.length > 0) {
-      $hiddenCustomerField.val($registerField.val());
-    }
-
-    const $hiddenCustomerField2 = $(
-      `#customer-fields input[name="${$registerField.attr('name')}"]`
-    );
-    if ($hiddenCustomerField2.length > 0) {
-      $hiddenCustomerField2.val($registerField.val());
-    }
-  });
-
-  let address_input = document.getElementById('google_address');
-  let options = {
-    types: ['address'],
-    componentRestrictions: {country: 'au'}
-  };
-  let autocomplete = new google.maps.places.Autocomplete(address_input, options);
-  autocomplete.addListener('place_changed', fillAddress);
-
-  function fillAddress() {
-    let place = autocomplete.getPlace();
-    let streetNumber,
-        streetName,
-        suburb,
-        city,
-        regionName,
-        regionCode,
-        postalCode;
-
-    place.address_components.forEach((item) => {
-      if (item.types.includes('street_number')) {
-        streetNumber = item.long_name;
-      }
-      if (item.types.includes('route')) {
-        streetName = item.long_name;
-      }
-      if (item.types.includes('locality')) {
-        suburb = item.long_name;
-      }
-      if (item.types.includes('administrative_area_level_2')) {
-        city = item.short_name;
-      }
-      if (item.types.includes('administrative_area_level_1')) {
-        regionName = item.long_name;
-        regionCode = item.short_name;
-      }
-      if (item.types.includes('postal_code')) {
-        postalCode = item.long_name;
-      }
-    });
-
-    $('input[name="customer[addresses][][address1]"]').val(`${streetNumber} ${streetName}`);
-    $('input[name="customer[addresses][][city]"]').val(`${suburb}`);
-    $('input[name="customer[postcode]"]').val(`${postalCode}`);
-    $('select[name="customer[addresses][][province]"]').val(regionName);
   }
 
-  $('#accountInfo form').find('input, select').trigger('change');
+  $('#settingAccountSpiner').show();
 
-  window.addSpace = function ($input) {
-    $input.val(`${$input.val()} `);
-  };
+  if (getCookie('is_from_registration')) {
+    $('.registration-confirm-message').show();
+    document.cookie = "is_from_registration=; max-age=0";
+  }
+
+  $('input, select').on('blur', function () {
+    $(this).removeClass('error');
+  });
+
+  $('#editDetails').click(function (e) {
+    e.preventDefault();
+    $('#detailsDisplay').fadeOut(200, function () {
+      $('#accountEdit').fadeIn();
+    });
+  });
+
+  $('.show_edit_form').click(function (e) {
+    e.preventDefault();
+    $('#editDetails').click();
+  });
+
+  $('#updateMarketing').click(function (e) {
+    e.preventDefault();
+    $('[data-marketing-hide]').hide();
+    $('[data-marketing-show]').show();
+    $('#detailsDisplay').fadeOut(200, function () {
+      $('#accountEdit').fadeIn();
+    });
+  });
+
+  $(document).on(
+    'click',
+    '#cancelEditAccount',
+    function (e) {
+      e.preventDefault();
+      $('#accountEdit').fadeOut(200, function () {
+        $('[data-marketing-hide]').show();
+        $('[data-marketing-show]').hide();
+        $('#detailsDisplay').fadeIn();
+      });
+    }
+  );
+
+  $(document).on(
+    'change',
+    '#cf_accept_marketing',
+    function (e) {
+      if ($('#cf_accept_marketing').is(':checked')) {
+        $('#cf_accept_marketing').prop("checked", true);
+        $('#cf_accept_marketing').val('true')
+      } else {
+        $('#cf_accept_marketing').prop("checked", false);
+        $('#cf_accept_marketing').val('')
+      }
+    }
+  );
+
+  document.addEventListener('cf:customer_saved', function (event) {
+    setTimeout(function () {
+      window.location.reload();
+    }, 2000)
+  });
+
+  var checkbox_accept_marketing = setInterval(function () {
+    if ($('[data-cf-loaded]').length) {
+      $('#settingAccountSpiner').hide();
+      if ($('#cf_accept_marketing').attr('checked')) {
+        $('#cf_accept_marketing').prop("checked", true);
+        $('#cf_accept_marketing').val('true');
+        clearInterval(checkbox_accept_marketing);
+      }
+    }
+  }, 100);
+
+  let hasError = false,
+    errorMessage = [],
+    $accept_terms = $('input[name="customer[accepts_terms_conditions]"]');
+
+  $('#cf_phone_number').on('input', function () {
+    this.value = parseInt(this.value.replace(/[^0-9]/g, ''), 10) || '';
+  });
+
+  $('input[name="customer[postcode]"]').on('input', function () {
+    this.value = parseInt(this.value.replace(/[^0-9]/g, ''), 10) || '';
+  });
+
+  function validate() {
+    errorMessage = [];
+    $('#cf_form_custom .required').each(function () {
+      if (!$(this).val() || $(this).val() === '') {
+        errorMessage.push('error: ' + $(this).attr("name"));
+        $(this).addClass('error');
+      } else {
+        $(this).removeClass('error');
+      }
+    })
+
+    if (!$('#cf_accept_terms').is(':checked')) {
+      $('#cf_accept_terms').addClass('error');
+      errorMessage.push('error');
+    } else {
+      $('#cf_accept_terms').removeClass('error')
+    }
+
+    hasError = errorMessage.length ? true : false;
+  }
+
+  function validateEmail(mail) {
+    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(mail)) {
+      return true
+    }
+    //alert("You have entered an invalid email address!")
+    return false
+  }
+
+  $('.btn-submit-cf').on('click', function (e) {
+
+    $('#cf_phone_number_hidden').val('+61' + $('#cf_phone_number').val())
+
+    const { customerEmail } = window.vars;
+
+    if ($('#accountInfo').length) {
+      document.cookie = "is_from_registration=1; max-age=" + 60 * 5;
+    }
+
+    validate();
+
+    if (!hasError) {
+
+      $('#accountEditSpiner').show();
+
+      $('html, body').animate({
+        scrollTop: 0
+      }, 100);
+      return true;
+    }
+
+    return false;
+  })
 
   $('.general-popup .modal_close').on('click', function () {
     const $popup = $(this).parents('.general-popup');
@@ -112,181 +187,46 @@ $(document).ready(() => {
     $popup.show();
   });
 
-  if ($('#accountEdit').length > 0) {
-    $('.edit-form .birthday_wrap').hide()
+  let address_input = document.getElementById('google_address');
+  let options = {
+    types: ['address'],
+    componentRestrictions: { country: 'au' }
+  };
+  let autocomplete = new google.maps.places.Autocomplete(address_input, options);
+  autocomplete.addListener('place_changed', fillAddress);
+  function fillAddress() {
+    let place = autocomplete.getPlace();
+    let streetNumber,
+      streetName,
+      suburb,
+      city,
+      regionName,
+      regionCode,
+      postalCode;
+    place.address_components.forEach((item) => {
+      if (item.types.includes('street_number')) {
+        streetNumber = item.long_name;
+      }
+      if (item.types.includes('route')) {
+        streetName = item.long_name;
+      }
+      if (item.types.includes('locality')) {
+        suburb = item.long_name;
+      }
+      if (item.types.includes('administrative_area_level_2')) {
+        city = item.short_name;
+      }
+      if (item.types.includes('administrative_area_level_1')) {
+        regionName = item.long_name;
+        regionCode = item.short_name;
+      }
+      if (item.types.includes('postal_code')) {
+        postalCode = item.long_name;
+      }
+    });
+    $('input[name="customer[addresses][address1]"]').val(`${streetNumber} ${streetName}`);
+    $('input[name="customer[addresses][city]"]').val(`${suburb}`);
+    $('input[name="customer[postcode]"]').val(`${postalCode}`);
+    $('select[name="customer[addresses][province]"]').val(regionName);
   }
-
-  $('input[name="customer[addresses][][phone]"]').on('input', function () {
-    this.value = parseInt(this.value.replace(/[^0-9]/g, ''), 10) || '';
-  });
-
-  $('input[name="customer[postcode]"]').on('input', function () {
-    this.value = parseInt(this.value.replace(/[^0-9]/g, ''), 10) || '';
-  });
-
-  $('[data-submit]').on('submit', function (e) {
-    let error = false;
-    const $email = $('input[name="customer[email]"]');
-
-    const $first_name = $('.copy-create input[name="customer[first_name]"]');
-    const $last_name = $('.copy-create input[name="customer[last_name]"]');
-    const $gender = $('.copy-create select[name="customer[gender]"]');
-    const $province = $('.copy-create select[name="customer[addresses][][province]"]');
-    const $suburb = $('.copy-create input[name="customer[addresses][][city]"]');
-    const $preferred_store = $('.copy-create select[name="customer[preferred_store]"]');
-    const $birthday = $('.copy-create input[name="customer[birthday]"]');
-    const $phone = $('.copy-create input[name="customer[addresses][][phone]"]');
-    const $address1 = $('.copy-create input[name="customer[addresses][][address1]"]');
-    const $post_code = $('.copy-create input[name="customer[postcode]"]');
-    const $terms_conditions = $('.copy-create input[name="customer[accepts_terms_conditions]"]');
-    const $accepts_marketing = $('.copy-create input[name="customer[accepts_marketing]"]');
-
-    const $realAcceptsMarketing = $('#customer-fields input[name="customer[accepts_marketing]"]');
-
-    const $falseGenderSelect = $('.copy-create select[name="customer[gender]"]');
-    const $falseStateSelect = $('.copy-create select[name="customer[addresses][][province]"]');
-    e.preventDefault();
-
-
-    if ($($(this).data('submit')).length > 0) {
-
-      if (!$first_name.val()) {
-        $first_name.addClass('error');
-        error = true;
-      } else {
-        $first_name.removeClass('error');
-      }
-
-      if (!$last_name.val()) {
-        $last_name.addClass('error');
-        error = true;
-      } else {
-        $last_name.removeClass('error');
-      }
-
-      if ($falseGenderSelect.find('option:selected').prop('disabled')) {
-        $falseGenderSelect.addClass('error');
-        error = true;
-      } else {
-        $falseGenderSelect.removeClass('error');
-      }
-
-      if (!$birthday.val()) {
-        $birthday.addClass('error');
-        error = true;
-      } else {
-        $birthday.removeClass('error');
-      }
-
-      if (!$phone.val()) {
-        $phone.addClass('error');
-        error = true;
-      } else {
-        $phone.removeClass('error');
-      }
-
-      if (!$address1.val()) {
-        $address1.addClass('error');
-        error = true;
-      } else {
-        $address1.removeClass('error');
-      }
-
-      if (!$post_code.val()) {
-        $post_code.addClass('error');
-        error = true;
-      } else {
-        $post_code.removeClass('error');
-      }
-
-      if (!$preferred_store.val()) {
-        $preferred_store.addClass('error');
-        error = true;
-      } else {
-        $preferred_store.removeClass('error');
-      }
-
-      if ($falseStateSelect.find('option:selected').prop('disabled')) {
-        $falseStateSelect.addClass('error');
-        error = true;
-      } else {
-        $falseStateSelect.removeClass('error');
-      }
-
-      if (!$suburb.val()) {
-        $suburb.addClass('error');
-        error = true;
-      } else {
-        $suburb.removeClass('error');
-      }
-
-      if (!$terms_conditions.is(':checked')) {
-        $terms_conditions.addClass('error')
-        error = true;
-      } else {
-        $terms_conditions.removeClass('error')
-      }
-
-      if (!error) {
-        const { customerEmail } = window.vars;
-
-        if ($realAcceptsMarketing.is(':checked')) {
-          const inFiveMinutes = new Date(new Date().getTime() + 5 * 60 * 1000);
-          Cookies.set('optedInAt', customerEmail, { expires: inFiveMinutes });
-        }
-
-        $email.val(customerEmail);
-
-        $($(this).data('submit')).submit();
-      }
-    }
-  });
-
-  $('#editDetails').click(function (e) {
-    e.preventDefault();
-    $('#detailsDisplay').fadeOut(200, function () {
-      $('#accountEdit').fadeIn();
-    });
-    $('#accountEdit form').find('input, select').trigger('change');
-  });
-
-  $('#updateMarketing').click(function (e) {
-    e.preventDefault();
-    $('[data-marketing-hide]').hide();
-    $('[data-marketing-show]').show();
-    $('#detailsDisplay').fadeOut(200, function () {
-      $('#accountEdit').fadeIn();
-    });
-    $('#accountEdit form').find('input, select').trigger('change');
-  });
-
-  $('#cancelEditAccount').click(function (e) {
-    e.preventDefault();
-    $('#accountEdit').fadeOut(200, function () {
-      $('[data-marketing-hide]').show();
-      $('[data-marketing-show]').hide();
-      $('#detailsDisplay').fadeIn();
-    });
-    $('#accountInfo form').find('input, select').trigger('change');
-  });
-
-  $('.show_edit_form').click(function (e) {
-    e.preventDefault();
-    $('#editDetails').click();
-  });
-
-  $('input, select').on('blur', function() {
-    $(this).removeClass('error');
-  });
-
-  const { showDetailsForm, customerEmail } = window.vars;
-  const optedIn = Cookies.get('optedInAt') === customerEmail;
-  if (showDetailsForm && !optedIn) {
-    $('#accountInfo').addClass('in');
-    $('.grid__item.fade').remove();
-  } else {
-    $('#accountInfo').remove();
-    $('.grid__item').addClass('in');
-  }
-  
-});
+})
