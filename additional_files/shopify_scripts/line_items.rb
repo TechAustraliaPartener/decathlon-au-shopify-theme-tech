9b848a1f37aa1afafe74ac6a3a85800554483b2b
John Martin <john@createur.com.au>
2021-04-12 13:39:42 +1000

    #7nt8ah - reject discount codes from array

--- additional_files/shopify_scripts/line_items.rb
+++ additional_files/shopify_scripts/line_items.rb
@@ -1,9 +1,9 @@
 cart = Input.cart
 
-REJECT_CODES = []
+REJECT_CODES = ['TESTREJECTED']
 REJECT_MESSAGE = "This discount code cannot be used for click & collect orders"
 
-if cart.discount_code
+if cart.discount_code && REJECT_CODES.include?(cart.discount_code.code)
   cart.discount_code.reject({ message: REJECT_MESSAGE })
 end
 
