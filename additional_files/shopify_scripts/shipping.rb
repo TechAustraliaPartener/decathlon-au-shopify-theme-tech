f571b87b9d4bea74cd437eb2d8ad0789e026ea6e
John Martin <john@createur.com.au>
2021-07-29 15:17:09 +1000

    #4gv9xm - add comments to shipping script

--- additional_files/shopify_scripts/shipping.rb
+++ additional_files/shopify_scripts/shipping.rb
@@ -11,6 +11,7 @@ shipping_rates.delete_if do |shipping_rate|
   if cart.total_weight > 19000
     rate_name_upcase == ("FREE STANDARD SHIPPING") or rate_name_upcase == ("EXPRESS SHIPPING")
   else
+    # Offer free standard shipping is cart total is greater than $79
     if cart.subtotal_price > Money.new(cents: 7900)
       rate_name_upcase == ("STANDARD SHIPPING")
     else
@@ -21,6 +22,9 @@ end
 
 puts shipping_rates.map { |rate| rate.name }
 
+# Delete Pickup if it is the only rate returned - 
+# if this occurs, it's probably an error, 
+# and we want to prevent this rate from being selected as the default.
 if shipping_rates.length == 1 && shipping_rates[0]
   shipping_rates.delete_if do |shipping_rate|
     shipping_rate.name.upcase.include? "PICKUP"
