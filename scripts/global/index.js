052573c2760da5fdf17e265906b22ecc0fcd0766
Michael Yap <michael@createur.com.au>
2021-09-02 13:55:34 +0800

    #6u6my0 - adjust logic for skeleton loader

--- scripts/global/index.js
+++ scripts/global/index.js
@@ -1,5 +1,22 @@
 (function() {
+
+  window.lazyFunctions = {
+    onTrendingCategoryImageLoad: function (element) {
+      const trendingCategoryTile = element.closest('.trending_category_tile');
+      trendingCategoryTile.classList.add('lazy_loaded');
+    },
+  };
+
+  function executeLazyFunction(element) {
+    var lazyFunctionName = element.getAttribute("data-lazy-function");
+    var lazyFunction = window.lazyFunctions[lazyFunctionName];
+    if (!lazyFunction) return;
+    lazyFunction(element);
+  }
+
   // Vanilla Lazyload
   // https://github.com/verlok/vanilla-lazyload#-getting-started
-  var lazyLoadInstance = new LazyLoad();
+  var lazyLoadInstance = new LazyLoad({
+    callback_loaded: executeLazyFunction
+  });
 })();
